956dbc3d4b84542b73e71444009aba841e046e15
Rudolph Kalp <rudolphk@lightstone.co.za>
2024-07-19 06:56:01 +0200

    Added task web api for b2c api inteegration

---
+++ TaskWebApi/Controllers/ValidationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValidationController : ControllerBase
+    {
+        [HttpPost("ValidateAccessCode")]
+        public async Task<IActionResult> ValidateAccessCode([FromBody] string accessCode)
+        {
+            // Validate the access code
+            if (accessCode == "54321")
+            {
+                return Ok();
+            }
+            else
+            {
+                return Conflict();
+            }
+        }
+    }
+}
