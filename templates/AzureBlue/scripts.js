809ee826d352a97d9aa0f4dfb8fa7d1e866f6c33
Rudolph Kalp <rudolphk@lightstone.co.za>
2024-08-13 10:55:02 +0200

    Updated scoped styling and MFA for phone numbers

--- templates/AzureBlue/scripts.js
+++ templates/AzureBlue/scripts.js
@@ -2,22 +2,20 @@ $(document).ready(function() {
     var url = new URL(window.location.href);
     var tenant = "";
     var stylesheet = "";
-    if (url.searchParams.has("client_id")) {
-        tenant = url.searchParams.get("client_id");
+    if (url.searchParams.has("tenantId")) {
+        tenant = url.searchParams.get("tenantId");
         if (tenant === "67524f15-f870-440d-a2d3-075264371001") {
             tenant = "agilebridge";
-            stylesheet = "https://ssotestsarfk.blob.core.windows.net/styles/agilebridge/abstyle.css";
+            stylesheet = "https://ssotestsarfk.blob.core.windows.net/styles/platform.css";
         }
-        else{
+        else {
             tenant = "lightstone";
-            stylesheet = "https://ssotestsarfk.blob.core.windows.net/styles/lightstone/lsstyle.css";
+            stylesheet = "https://ssotestsarfk.blob.core.windows.net/styles/platform.css";
         }
-    }
-    else if (url.searchParams.has("tenant")) {
-        tenant = url.searchParams.get("tenant");
     }
     else {
-        tenant = "default";
+        tenant = "lightstone";
+        stylesheet = "https://ssotestsarfk.blob.core.windows.net/styles/platform.css";
     }
     // var styleSheet = 'https://cdn.test.platform.coaxle.co.za/tenantmanagement-api-test/styles/tenant.css?tenantId=' + tenantId;
     document.getElementsByTagName("head")[0].insertAdjacentHTML(
