1c94021ccae8e43a3092f9e1d1a5c3614a138741
Rudolph Kalp <rudolphk@lightstone.co.za>
2024-08-23 13:01:42 +0200

    Updated flow and styling and minimized the styling sheet

--- templates/AzureBlue/scripts.js
+++ templates/AzureBlue/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var url = new URL(window.location.href);
     var tenant = "";
     var stylesheet = "";
@@ -22,3 +22,146 @@ $(document).ready(function() {
         "beforeend",
         "<link rel='stylesheet' href='" + stylesheet + "' />");
 });
+
+// var iti;
+// var lip;
+
+// function setupUi() {
+//     //hide country code
+//     document.getElementById('mfaCountryCode').style.visibility = 'hidden';
+//     //hide the change button
+//     if (document.getElementById('phoneVerificationControl-readOnly_but_change_claims') !== null) {
+//         document.getElementById('phoneVerificationControl-readOnly_but_change_claims').style.visibility = 'hidden';
+//         continueLogin();
+//     }
+//     if (document.getElementById('phoneVerificationControl_but_change_claims') !== null) {
+//         document.getElementById('phoneVerificationControl_but_change_claims').style.visibility = 'hidden';
+//     }
+
+//     if (document.getElementById('mfaPhoneNumber') == null) {
+//         setTimeout(() => setupUi(), 2000);
+//     }
+//     else {
+
+//         document.getElementById('mfaPhoneNumber').hidden = true;
+//         let div = document.createElement('div');
+//         let intelInputElement = document.createElement('input');
+//         intelInputElement.id = 'phoneNumner';
+//         intelInputElement.name = 'phoneNumner';
+//         intelInputElement.type = 'tel';
+//         intelInputElement.className = 'textInput';
+//         div.append(intelInputElement);
+//         document.querySelector('.TextBox.Phone > .attrEntry').append(div);
+
+//         checkIfReady();
+//     }
+// }
+
+function continueLogin() {
+    //hide button and continue login
+    // document.getElementById('continue').style.visibility = 'hidden';
+    document.getElementById("continue").click();
+}
+
+
+function configure() {
+    iti = intlTelInput(document.getElementById('phoneNumner'), {
+        initialCountry: "za",
+        preferredCountries: ['za']
+    });
+
+    document.getElementById('phoneNumner').addEventListener('countrychange', function () {
+        setNumber();
+        validateNumber();
+    });
+
+    document.getElementById('phoneNumner').addEventListener('change', function () {
+        setNumber();
+        validateNumber();
+    });
+
+    document.getElementById('phoneNumner').addEventListener('keyup', function () {
+        setNumber();
+        validateNumber();
+    });
+
+    setNumber();
+}
+
+function setNumber() {
+    var number = iti.telInput.value;
+    if (number.indexOf('0') === 0) {
+        number = number.substring(1, number.length);
+        document.getElementById('phoneNumner').value = number;
+    }
+    document.getElementById('mfaPhoneNumber').value = '+' + iti.getSelectedCountryData().dialCode + number;
+
+    //Set the country code
+    document.getElementById('mfaCountryCode').value = iti.getSelectedCountryData().iso2;
+
+}
+
+function validateNumber() {
+    document.getElementById('phoneVerificationControl_but_send_code').style.visibility = 'hidden';
+    let isValidForCountry = libphonenumber.isValidPhoneNumber(document.getElementById('phoneNumner').value, iti.getSelectedCountryData().iso2.toUpperCase())
+    let numberLeng = Math.ceil(Math.log10(document.getElementById('phoneNumner').value));
+
+    if (isValidForCountry) {
+        if (iti.getSelectedCountryData().iso2.toUpperCase() == "ZA" && numberLeng < 9) {
+            showInValidNumberStyle();
+        } else {
+            showValidNumberStyle();
+        }
+    } else {
+        showInValidNumberStyle();
+    }
+}
+
+function showInValidNumberStyle() {
+    document.getElementsByClassName("intro")[0].textContent = 'Please provide a valid number ';
+    document.getElementsByClassName("intro")[0].style.color = 'red'
+    document.getElementById("phoneNumner").style.color = 'red'
+    document.getElementById('phoneVerificationControl_but_verify_code').style.visibility = 'hidden';
+    document.getElementById('phoneVerificationControl_but_send_new_code').style.visibility = 'hidden';
+}
+function showValidNumberStyle() {
+    document.getElementById('phoneVerificationControl_but_send_code').style.visibility = 'visible';
+    document.getElementsByClassName("intro")[0].style.color = '#002072'
+    document.getElementById("phoneNumner").style.color = '#002072'
+    document.getElementById('phoneVerificationControl_but_verify_code').style.visibility = 'visible';
+    document.getElementById('phoneVerificationControl_but_send_new_code').style.visibility = 'visible';
+}
+
+function checkIfReady() {
+    try {
+        configure();
+    } catch {
+        setTimeout(() => {
+            checkIfReady();
+        }, 100);
+    }
+}
+
+function isSignUpReadyForLogin() {
+
+    //this is for the register of new user
+    //$('#phoneVerificationControl_code_sent_message').attr("aria-hidden")
+    if ($('#phoneVerificationControl_code_sent_message').attr("aria-hidden") == 'true' && document.getElementById('phoneVerificationControl_but_verify_code').style.display == 'none' && document.getElementById('phoneVerificationControl_but_send_code').style.display == 'none' && document.getElementById("continue") !== undefined) {
+        continueLogin();
+    } else {
+        setTimeout(() => checkIfSignUpIsReady(), 500);
+    }
+}
+
+function checkIfSignUpIsReady() {
+    try {
+        isSignUpReadyForLogin();
+    } catch {
+        setTimeout(() => {
+            isSignUpReadyForLogin();
+        }, 100);
+    }
+}
+
+// setupUi();
+checkIfSignUpIsReady();
